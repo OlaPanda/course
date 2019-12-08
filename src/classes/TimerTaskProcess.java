2cbf83b471383a65acb2abf7e4f3311a06282548
OlaPanda <38304252+OlaPanda@users.noreply.github.com>
2019-12-08 21:29:08 +0200

    Initial commit

---
+++ src/classes/TimerTaskProcess.java
@@ -0,0 +1,21 @@
+package classes;
+
+import com.company.Controller;
+
+import java.util.TimerTask;
+
+public class TimerTaskProcess extends TimerTask {
+    private Processes processes;
+    private GenerateProcesses generateProcesses = new GenerateProcesses();
+
+
+    public TimerTaskProcess(Processes processes){
+        this.processes = processes;
+    }
+
+    @Override
+    public void run() {
+        generateProcesses.generate(processes);
+        Controller.Refresh();
+    }
+}
